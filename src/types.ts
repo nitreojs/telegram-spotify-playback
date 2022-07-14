d0ca109cd1258467796a8614f12fb57af87ff605
starkow <hello@starkow.dev>
2022-07-14 17:47:07 +0300

    refactor: expose env types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,21 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface ProcessEnv {
+      SPOTIFY_CLIENT_ID: string
+      SPOTIFY_CLIENT_SECRET: string
+      SPOTIFY_ACCESS_TOKEN: string
+      SPOTIFY_REFRESH_TOKEN: string
+      SPOTIFY_REDIRECT_URI: string
+
+      TELEGRAM_BOT_TOKEN: string
+      TELEGRAM_CHANNEL_IDS: string // TODO: deprecate
+
+      LASTFM_API_KEY: string
+    }
+  }
+}
+
 export interface YamlDataChannel {
   id: number
   message_id: number
